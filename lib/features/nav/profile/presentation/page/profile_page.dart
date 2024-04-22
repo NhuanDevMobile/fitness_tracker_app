f4d443a7b44bff95becba2f96133ccd4b59d9d68
Nhuan DEV <nhuandevmobile@gmail.com>
2024-04-22 08:14:22 +0700

    ui profile

--- lib/features/nav/profile/presentation/page/profile_page.dart
+++ lib/features/nav/profile/presentation/page/profile_page.dart
@@ -1,5 +1,5 @@
-import 'package:fitness_tracker_app/core/routes/routes.dart';
-import 'package:fitness_tracker_app/core/ui/widgets/appbar/appbar_widget.dart';
+import 'package:fitness_tracker_app/core/configs/app_colors.dart';
+import 'package:fitness_tracker_app/core/ui/widgets/text/text_widget.dart';
 import 'package:fitness_tracker_app/features/nav/profile/presentation/controller/profile_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,36 +9,78 @@ class ProfilePage extends GetView<ProfileController> {
 
   @override
   Widget build(BuildContext context) {
+    var top = 0.0;
     return Scaffold(
-      appBar: const AppBarWidget(
-        leading: SizedBox.shrink(),
-        title: "profile",
-        centerTitle: true,
+        body: NestedScrollView(
+      headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+        return <Widget>[
+          SliverAppBar(
+              backgroundColor: AppColors.primary,
+              expandedHeight: 200.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                // print('constraints=' + constraints.toString());
+                top = constraints.biggest.height;
+                return FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: const AnimatedOpacity(
+                    duration: Duration(milliseconds: 300),
+                    //opacity: top == MediaQuery.of(context).padding.top + kToolbarHeight ? 1.0 : 0.0,
+                    opacity: 1.0,
+                    child: Row(
+                      children: [
+                        TextWidget(text: "text"),
+                      ],
+                    ),
+                  ),
+                  background: Container(
+                    child: const Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextWidget(text: "text"),
+                      ],
+                    ),
+                  ),
+                );
+              })),
+        ];
+      },
+      body: SingleChildScrollView(
+        child: Container(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(height: 16),
-            const SizedBox(height: 8),
-            const Text(
-              'Software Developer',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                controller.handleLogout();
-                Get.offAndToNamed(Routes.login);
-              },
-              child: const Text('Logout'),
-            ),
-          ],
-        ),
-      ),
-    );
+    ));
+    // return Scaffold(
+    //   appBar: const AppBarWidget(
+    //     leading: SizedBox.shrink(),
+    //     title: "profile",
+    //     centerTitle: true,
+    //   ),
+    //   body: Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: [
+    //         const SizedBox(height: 16),
+    //         const SizedBox(height: 8),
+    //         const Text(
+    //           'Software Developer',
+    //           style: TextStyle(
+    //             fontSize: 16,
+    //             color: Colors.grey,
+    //           ),
+    //         ),
+    //         const SizedBox(height: 16),
+    //         ElevatedButton(
+    //           onPressed: () {
+    //             controller.handleLogout();
+    //             Get.offAndToNamed(Routes.login);
+    //           },
+    //           child: const Text('Logout'),
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 }
